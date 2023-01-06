09644a936f18f2ae6c9e1f66044c4a36b0b5bd94
Sumit singh <sumit700490@gmail.com>
2023-01-07 00:38:25 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,13 @@
+//Given 3 numbers (all different values), print which is greatest
+
+let a = 5;
+let b = 7;
+let c= 8;
+
+if(a>b){
+  console.log("Great");
+}else if(b>c){
+  console.log("Greater");
+}else if(c>a){
+  console.log("Greatest");
+}
