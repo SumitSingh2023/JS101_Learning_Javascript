bf917364a6c0d204109cef6c00b79ea1fbae9213
SumitSingh2023 <121820847+SumitSingh2023@users.noreply.github.com>
2023-01-07 01:33:27 +0530

    Add files via upload

---
+++ problem4.js
@@ -0,0 +1,12 @@
+//Given 2 numbers a and b print which is greater or "both equal".
+
+let a = 5;
+let b = 5;
+
+if((a<b) || (a>b)){
+  console.log("greater");
+}else{
+  console.log("both equal");
+}
+
+
